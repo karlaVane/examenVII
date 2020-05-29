49c1e0fc258b4d05e2db4e006d676ec9c8ca70af
karlaVane <kmoyonr@est.ups.edu.ec>
2020-05-29 11:24:28 -0500

    Examen finalizado.

--- crear/crearArch.js
+++ crear/crearArch.js
@@ -3,17 +3,18 @@ const fs = require('fs');
 let crearArchivo = (pmedia, anio, pais) => {
     let datos;
     return new Promise((resolve, reject) => {
-        datos += `Datos: ${pmedia[3]}\n`;
-        datos += `País: ${pmedia[1]}\n`;
-        datos += `Año: ${anio}\n`;
-        datos += `Valor: ${pmedia[0]}`;
+        datos = `ARCHIVO DE ${pmedia[1]} EN EL AÑO ${anio} \n`
+        datos += `*Datos: ${pmedia[3]}\n`;
+        datos += `*País: ${pmedia[1]}\n`;
+        datos += `*Año: ${anio}\n`;
+        datos += `*Valor: ${pmedia[0]}`;
 
-        console.log(`Nombre del archivo: ${pais}-${anio}.txt`);
+        //console.log(`Nombre del archivo: ${pais}-${anio}.txt`);
         fs.writeFile(`resultados/${pais}-${anio}.txt`, datos, (err) => {
             if (err)
                 reject(err);
             else
-                resolve(`EL archivo del pais ${pais} en el año ${anio}  se a Guardado`);
+                resolve(`Revisar archivo guardado en /resultados/${pais}-${anio}.txt`);
         });
     });
 }
